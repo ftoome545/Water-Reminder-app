b29cffb6d438c76c602feb3588f3363f62dde88b
FatimaHure <ftoome7767@hotmail.com>
2023-07-07 09:39:09 +0300

    add firebase and fix the error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import '../screens/start_page.dart';
 
-void main() {
+// void main() {
+//   runApp(MaterialApp(home: const MyApp()));
+// }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(home: const MyApp()));
 }
 

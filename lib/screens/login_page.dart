5af4173b94d3110f12bb8f190faee29cdaf3e182
FatimaHure <ftoome7767@hotmail.com>
2023-07-14 19:35:59 +0300

    add login things and showSpinner for loading icon

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:water_reminder_app/screens/reset_password.dart';
-import 'package:water_reminder_app/screens/signUp_page.dart';
+import 'package:water_reminder_app/screens/user_info.dart';
+import '../screens/reset_password.dart';
+import '../screens/signUp_page.dart';
 import '../widgets/email_password.dart';
 import 'home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,145 +16,176 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _auth = FirebaseAuth.instance;
+  late String email;
+  late String password;
+  bool showSpinner = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('images/login_image.jpg'),
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 7, 96, 168)),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/login_image.jpg'),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 7, 96, 168)),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: EmailPassword(
-                title: 'Email',
-                hint: 'Enter your email',
-                onchanged: (value) {},
-                obscureText: false,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-              child: EmailPassword(
-                title: 'Password',
-                hint: 'Enter your password',
-                onchanged: (value) {},
-                obscureText: true,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 8.0,
-                left: 215.0,
-              ),
-              // child: Text(
-              //   'Forgot Password?',
-              //   style: TextStyle(
-              //       fontSize: 18, color: Color.fromARGB(255, 7, 96, 168)),
-              // ),
-              child: RichText(
-                text: TextSpan(
-                    text: 'Forgot Passsword?',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Color.fromARGB(255, 7, 96, 168),
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ResetPassword()));
-                      }),
-              ),
-            ),
-            Stack(
-              children: [
-                Image.asset(
-                  'images/login_image_2.jpg',
-                  width: 563.0,
-                  height: 270.5,
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: EmailPassword(
+                  title: 'Email',
+                  hint: 'Enter your email',
+                  onchanged: (value) {
+                    email = value;
+                  },
+                  obscureText: false,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 200.0,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                child: EmailPassword(
+                  title: 'Password',
+                  hint: 'Enter your password',
+                  onchanged: (value) {
+                    password = value;
+                  },
+                  obscureText: true,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  left: 215.0,
+                ),
+                // child: Text(
+                //   'Forgot Password?',
+                //   style: TextStyle(
+                //       fontSize: 18, color: Color.fromARGB(255, 7, 96, 168)),
+                // ),
+                child: RichText(
+                  text: TextSpan(
+                      text: 'Forgot Passsword?',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 7, 96, 168),
                       ),
-                      child: RichText(
-                        text: TextSpan(
-                            text: 'New here?',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                            children: [
-                              TextSpan(
-                                  text: ' Sign Up',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  recognizer: TapGestureRecognizer()
-                                    ..onTap = () {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const SignUpPage()));
-                                    }),
-                            ]),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ResetPassword()));
+                        }),
+                ),
+              ),
+              Stack(
+                children: [
+                  Image.asset(
+                    'images/login_image_2.jpg',
+                    width: 563.0,
+                    height: 270.5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 200.0,
+                        ),
+                        child: RichText(
+                          text: TextSpan(
+                              text: 'New here?',
+                              style: const TextStyle(
+                                  fontSize: 18.0, color: Colors.white),
+                              children: [
+                                TextSpan(
+                                    text: ' Sign Up',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const SignUpPage()));
+                                      }),
+                              ]),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 130.0,
-                        left: 55.0,
-                      ),
-                      child: SizedBox(
-                        width: 130.8,
-                        height: 57.14,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                              side: const BorderSide(
-                                  color: Colors.white, width: 2),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 130.0,
+                          left: 55.0,
+                        ),
+                        child: SizedBox(
+                          width: 130.8,
+                          height: 57.14,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                                side: const BorderSide(
+                                    color: Colors.white, width: 2),
+                              ),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 7, 107, 132),
                             ),
-                            backgroundColor:
-                                const Color.fromARGB(255, 7, 107, 132),
-                          ),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const HomePage()));
-                          },
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(fontSize: 24),
+                            onPressed: () async {
+                              setState(() {
+                                showSpinner = true;
+                              });
+                              try {
+                                final user =
+                                    await _auth.signInWithEmailAndPassword(
+                                        email: email, password: password);
+                                if (user != null) {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              UserInfoPage()));
+                                  setState(() {
+                                    showSpinner = false;
+                                  });
+                                }
+                              } catch (e) {
+                                print(e);
+                              }
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => const HomePage()));
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(fontSize: 24),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signUp_page.dart
+++ lib/screens/signUp_page.dart
@@ -4,6 +4,7 @@ import '../screens/user_info.dart';
 import '../screens/login_page.dart';
 import '../widgets/email_password.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -17,141 +18,151 @@ class _SignUpPageState extends State<SignUpPage> {
   late String userName;
   late String email;
   late String password;
+  bool showSpinner = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('images/login_image.jpg'),
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 7, 96, 168)),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/login_image.jpg'),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 7, 96, 168)),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: EmailPassword(
-                title: 'Full Name',
-                hint: 'Fatima Hure',
-                onchanged: (value) {
-                  userName = value;
-                },
-                obscureText: false,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: EmailPassword(
-                title: 'Email',
-                hint: 'fatimahure@gmail.com',
-                onchanged: (value) {
-                  email = value;
-                },
-                obscureText: false,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: EmailPassword(
-                title: 'Password',
-                hint: 'Enter your password',
-                onchanged: (value) {
-                  password = value;
-                },
-                obscureText: true,
-              ),
-            ),
-            Stack(
-              children: [
-                Image.asset(
-                  'images/login_image_2.jpg',
-                  width: 563.0,
-                  height: 270.5,
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: EmailPassword(
+                  title: 'Full Name',
+                  hint: 'Fatima Hure',
+                  onchanged: (value) {
+                    userName = value;
+                  },
+                  obscureText: false,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 200.0,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: EmailPassword(
+                  title: 'Email',
+                  hint: 'fatimahure@gmail.com',
+                  onchanged: (value) {
+                    email = value;
+                  },
+                  obscureText: false,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: EmailPassword(
+                  title: 'Password',
+                  hint: 'Enter your password',
+                  onchanged: (value) {
+                    password = value;
+                  },
+                  obscureText: true,
+                ),
+              ),
+              Stack(
+                children: [
+                  Image.asset(
+                    'images/login_image_2.jpg',
+                    width: 563.0,
+                    height: 270.5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 200.0,
+                        ),
+                        child: RichText(
+                          text: TextSpan(
+                              text: 'Already Member?',
+                              style: const TextStyle(
+                                  fontSize: 18.0, color: Colors.white),
+                              children: [
+                                TextSpan(
+                                    text: ' Login',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const LoginPage()));
+                                      }),
+                              ]),
+                        ),
                       ),
-                      child: RichText(
-                        text: TextSpan(
-                            text: 'Already Member?',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                            children: [
-                              TextSpan(
-                                  text: ' Login',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  recognizer: TapGestureRecognizer()
-                                    ..onTap = () {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const LoginPage()));
-                                    }),
-                            ]),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 130.0,
-                        left: 30.0,
-                      ),
-                      child: SizedBox(
-                        width: 130.8,
-                        height: 57.14,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                              side: const BorderSide(
-                                  color: Colors.white, width: 2),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 130.0,
+                          left: 30.0,
+                        ),
+                        child: SizedBox(
+                          width: 130.8,
+                          height: 57.14,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                                side: const BorderSide(
+                                    color: Colors.white, width: 2),
+                              ),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 7, 107, 132),
                             ),
-                            backgroundColor:
-                                const Color.fromARGB(255, 7, 107, 132),
-                          ),
-                          onPressed: () async {
-                            try {
-                              final newUser =
-                                  await _auth.createUserWithEmailAndPassword(
-                                      email: email, password: password);
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => UserInfoPage()));
-                            } catch (e) {
-                              print(e);
-                            }
-                          },
-                          child: const Text(
-                            'Sign Up',
-                            style: TextStyle(fontSize: 24),
+                            onPressed: () async {
+                              setState(() {
+                                showSpinner = true;
+                              });
+                              try {
+                                final newUser =
+                                    await _auth.createUserWithEmailAndPassword(
+                                        email: email, password: password);
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => UserInfoPage()));
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                              } catch (e) {
+                                print(e);
+                              }
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(fontSize: 24),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

ebcaed80a021f6ca8f753176067e0672e211ef09
FatimaHure <ftoome7767@hotmail.com>
2023-07-25 00:23:24 +0300

    work on delete one of today's drunks record

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,8 +16,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late ValueNotifier<List<DrinkRecordModel>> _items;
   double amount = 0;
   late double recommendedAmount;
+
+  void initState() {
+    super.initState();
+    _items = ValueNotifier([
+      DrinkRecordModel(
+        time: '11:0 AM',
+        amountOfWater: '${widget.unit == 'kilograms' ? '175 ml' : '6 fl oz'}',
+      ),
+      DrinkRecordModel(
+        time: '03:44 PM',
+        amountOfWater: '${widget.unit == 'kilograms' ? '175 ml' : '6 fl oz'}',
+      ),
+      DrinkRecordModel(
+        time: '12:44 AM',
+        amountOfWater: '${widget.unit == 'kilograms' ? '175 ml' : '6 fl oz'}',
+      ),
+    ]);
+  }
 
   double calRecommAmount() {
     widget.unit == 'kilograms'
@@ -30,85 +49,77 @@ class _HomeState extends State<Home> {
     widget.unit == 'kilograms' ? '175 ml' : '6 fl oz';
   }
 
+  void _deleteItem(int index) {
+    setState(() {
+      _items.value.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // Padding(
-          //   padding: const EdgeInsets.only(top: 48.0),
-          //   child: ListTile(
-          //     leading: Icon(
-          //       Icons.arrow_back,
-          //       color: const Color.fromARGB(255, 7, 107, 132),
-          //       size: 29,
-          //     ),
-          //     onTap: () {
-          //       Navigator.pushReplacement(
-          //           context,
-          //           MaterialPageRoute(
-          //               builder: (context) => const AddUserInfo()));
-          //     },
-          //   ),
-          // ),
-          Padding(
-            padding: EdgeInsets.only(top: 60, left: 33, right: 33, bottom: 5),
-            child: SizedBox(
-              width: 295,
-              height: 238,
-              child: Card(
-                color: Color.fromRGBO(0, 200, 250, 0.415),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 46, bottom: 21, left: 62, right: 62),
-                      child: Text(
-                        'Your Drink Goal',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 21, left: 52, right: 52),
-                      child: Text(
-                        '$amount / ${widget.unit == 'kilograms' ? recommendedAmount = widget.weight * 30 : recommendedAmount = widget.weight * 0.5} ${widget.unit == 'kilograms' ? 'ml' : 'oz'}',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 20, left: 57, right: 57),
-                      child: ListTile(
-                        title: Text(
-                          '${widget.unit == 'kilograms' ? '175 ml' : '6 fl oz'}',
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(top: 90, left: 33, right: 33, bottom: 5),
+              child: SizedBox(
+                width: 295,
+                height: 238,
+                child: Card(
+                  color: Color.fromRGBO(0, 200, 250, 0.415),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 46, bottom: 21, left: 62, right: 62),
+                        child: Text(
+                          'Your Drink Goal',
                           style: TextStyle(
-                            fontSize: 18,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        leading: Icon(
-                          Icons.coffee_sharp,
-                          size: 70,
-                        ),
-                        onTap: () {
-                          setState(() {
-                            widget.unit == 'kilograms'
-                                ? amount += 175
-                                : amount += 6;
-                          });
-                        },
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 21, left: 52, right: 52),
+                        child: Text(
+                          '$amount / ${widget.unit == 'kilograms' ? recommendedAmount = widget.weight * 30 : recommendedAmount = widget.weight * 0.5} ${widget.unit == 'kilograms' ? 'ml' : 'oz'}',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 20, left: 57, right: 57),
+                        child: ListTile(
+                          title: Text(
+                            '${widget.unit == 'kilograms' ? '175 ml' : '6 fl oz'}',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          leading: Icon(
+                            Icons.coffee_sharp,
+                            size: 70,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              widget.unit == 'kilograms'
+                                  ? amount += 175
+                                  : amount += 6;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -133,43 +144,49 @@ class _HomeState extends State<Home> {
           ),
           Padding(
             padding:
-                const EdgeInsets.only(top: 15, bottom: 5, left: 5, right: 170),
-            child: ListTile(
-              title: Icon(Icons.add_circle_outline),
-              leading: Text(
+                const EdgeInsets.only(top: 15, bottom: 5, left: 15, right: 170),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
                 "Today's drunks",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              onTap: () {},
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15.0),
-            child: SizedBox(
-              height: 155,
-              width: 350,
-              child: Card(
-                color: Color.fromARGB(255, 220, 239, 249),
-                shadowColor: const Color.fromARGB(14, 0, 0, 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: Column(
-                  children: [
-                    DrinkRecord(
-                      amountOfWater: '175 ml',
-                      time: '03:44 PM',
-                      onTab: () {},
-                    ),
-                    DrinkRecord(
-                      amountOfWater: '175 ml',
-                      time: '03:44 PM',
-                      onTab: () {},
-                    ),
-                  ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0, bottom: 15),
+              child: SizedBox(
+                height: 155,
+                width: 350,
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  color: Color.fromARGB(255, 220, 239, 249),
+                  shadowColor: const Color.fromARGB(14, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: ValueListenableBuilder<List<DrinkRecordModel>>(
+                    valueListenable: _items,
+                    builder: (context, items, child) {
+                      return ListView.builder(
+                        padding: const EdgeInsets.only(top: 15),
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          return DrinkRecord(
+                            time: items[index].time,
+                            amountOfWater: items[index].amountOfWater,
+                            onDelete: () {
+                              _deleteItem(index);
+                            },
+                          );
+                        },
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
--- lib/widgets/drink_record.dart
+++ lib/widgets/drink_record.dart
@@ -5,59 +5,68 @@ class DrinkRecord extends StatelessWidget {
     super.key,
     required this.time,
     required this.amountOfWater,
-    required this.onTab,
+    required this.onDelete,
   });
 
   final String time;
   final String amountOfWater;
-  final Function onTab;
+  final VoidCallback onDelete;
 
   void actionPopUpItemSelected(String value) {
-    String message;
     if (value == 'edit') {
-      message = 'You selected edit item ';
       // You can navigate the user to edit page.
     } else if (value == 'delete') {
-      message = 'You selected delete item ';
       // You can delete the item.
-    } else {
-      message = 'Not implemented';
-    }
-    print(message);
+      onDelete();
+    } else {}
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 22, right: 22),
-      child: ListTile(
-        title: Text(amountOfWater),
-        trailing: PopupMenuButton(
-          onSelected: (value) {
-            print('You Click on po up menu item');
-            actionPopUpItemSelected(value);
-          },
-          itemBuilder: (context) {
-            return [
-              PopupMenuItem(
-                value: 'delete',
-                child: Text('Delete'),
+      child: Column(
+        children: [
+          ListTile(
+            contentPadding: EdgeInsets.only(),
+            title: Text(amountOfWater),
+            trailing: PopupMenuButton(
+              onSelected: (value) {
+                actionPopUpItemSelected(value);
+              },
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    value: 'delete',
+                    child: Text('Delete'),
+                  ),
+                  PopupMenuItem(
+                    value: 'edit',
+                    child: Text('Edit'),
+                  ),
+                ];
+              },
+            ),
+            leading: Text(
+              time,
+              style: TextStyle(
+                fontSize: 18,
               ),
-              PopupMenuItem(
-                value: 'edit',
-                child: Text('Edit'),
-              ),
-            ];
-          },
-        ),
-        leading: Text(
-          time,
-          style: TextStyle(
-            fontSize: 18,
+            ),
+            onTap: () {},
           ),
-        ),
-        onTap: onTab(),
+          SizedBox(height: 0),
+        ],
       ),
     );
   }
+}
+
+class DrinkRecordModel {
+  final String time;
+  final String amountOfWater;
+  DrinkRecordModel({
+    required this.time,
+    required this.amountOfWater,
+  });
 }
